592bd83cb7ee9a4d28a8f538cc8adf4482185124
ThibaultPirson <thibault.pirson@uclouvain.be>
2020-04-01 09:39:28 +0200

    update pin list due to a hardware inversion on the interface board layout

--- src/hal/pins.c
+++ src/hal/pins.c
@@ -33,9 +33,9 @@ struct io_pin_config DIO_PIN_CONFIG[] = {
     {DIO_PIN_BUTTON_UP,               IO_PORTA, 2},
     {DIO_PIN_BUTTON_DOWN,             IO_PORTA, 3},
     {DIO_PIN_BUTTON_VTIDAL_UP,        IO_PORTA, 4},
-    {DIO_PIN_BUTTON_VTIDAL_DOWN,      IO_PORTA, 5},
-    {DIO_PIN_BUTTON_FREQ_RESPI_DOWN,  IO_PORTA, 6},
-    {DIO_PIN_BUTTON_FREQ_RESPI_UP,    IO_PORTA, 7},
+    {DIO_PIN_BUTTON_VTIDAL_DOWN,      IO_PORTA, 7},
+    {DIO_PIN_BUTTON_FREQ_RESPI_DOWN,  IO_PORTA, 5},
+    {DIO_PIN_BUTTON_FREQ_RESPI_UP,    IO_PORTA, 6},
 
     {DIO_PIN_LCD_EN,                  IO_PORTC, 4},
     {DIO_PIN_LCD_RS,                  IO_PORTC, 5},
