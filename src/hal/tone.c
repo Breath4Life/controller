e203b3d8fcb12a666a58b12dc974600c2d0f0ed2
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2020-04-07 20:59:21 +0200

    tone: add second pin output on PWM

--- src/hal/tone.c
+++ src/hal/tone.c
@@ -1,22 +1,31 @@
+#include "hal/pins.h"
+#include "hal/io.h"
 #include "hal/tone.h"
 #define TONE_MULT F_CPU/256/2
 
 void tone_init()
 {
-    TCCR0A = (1<<WGM01); // WGM01 : CTC mode, COM0A0 : Output toggle OC0A
-    TCCR0B = 1<<CS02; // with 256 prescler
+    dio_init(DIO_PIN_ALARM_SOUND, DIO_OUTPUT);
+    dio_init(DIO_PIN_ALARM_SOUND_BIS, DIO_OUTPUT);
+    dio_write(DIO_PIN_ALARM_SOUND, DIO_LOW);
+    dio_write(DIO_PIN_ALARM_SOUND_BIS, DIO_LOW);
+    TCCR0A = _BV(WGM01); // WGM01 : CTC mode, COM0A0 : Output toggle OC0A
+    TCCR0B = _BV(CS02); // with 256 prescaler
 }
 
 void tone_start(uint16_t freq)
 {
     // For frequencies of the alarm : 260 - 523
     // OCR0A will take 58 -> 119
-    OCR0A = (TONE_MULT/freq)-1;
-    TCCR0A = (1<<COM0A0)|(1<<WGM01); // WGM01 : CTC mode, COM0A0 : Output toggle OC0A
+    uint8_t period =(TONE_MULT/freq)-1;
+    OCR0A = period;
+    OCR0B = period;
+    // WGM01 : CTC mode, COM0A0 | COM0B0 : Output toggle OC0A and OC0B
+    TCCR0A = _BV(COM0A0) | _BV(COM0B0) | _BV(WGM01);
 }
 
 void tone_stop()
 {
-    // TODO check that result is low-level
-    TCCR0A = (1<<WGM01); // WGM01 : CTC mode, COM0A0 : Output toggle OC0A
+    // WGM01 : CTC mode, with COMA and COMB outputs in 'normal' DIO mode
+    TCCR0A = _BV(WGM01);
 }
--- src/main.c
+++ src/main.c
@@ -54,8 +54,6 @@ void initHardware(void)
 
     dio_init(DIO_PIN_AUX_ALARM_GATE_PIN,      DIO_INPUT);
 
-    dio_init(DIO_PIN_ALARM_SOUND,             DIO_OUTPUT);
-    dio_init(DIO_PIN_ALARM_SOUND_BIS,         DIO_OUTPUT);
     dio_init(DIO_PIN_ALARM_LED_HPA,           DIO_OUTPUT);
     dio_init(DIO_PIN_ALARM_LED_LPA,           DIO_OUTPUT);
     dio_init(DIO_PIN_STEP_COUNTER_TN,         DIO_OUTPUT);
