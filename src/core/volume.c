fc7645e86021a5d38a97708d861c69f250c657cd
anpar <paris.antoine.paris@gmail.com>
2020-04-10 13:02:37 +0200

    Add debug_print in volume.c

--- src/core/volume.c
+++ src/core/volume.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include "core/volume.h"
+#include "core/debug.h"
 #include "hal/sfm3000.h"
 #include "hal/time.h"
 
@@ -10,13 +11,25 @@ volatile int32_t volume;
 // Last poll time
 uint32_t last_poll_time;
 
+#define DEBUG_FLOW 0
+
+#if DEBUG_FLOW
+uint32_t i = 0;
+#endif
+
 void init_volume() {
+#if DEBUG_FLOW
+    debug_print("[FLOW] Init.\r\n");
+#endif
     sfm3000_init(1);
     volume = 0;
     last_poll_time = time_us();
 }
 
 uint8_t poll_volume() {
+#if DEBUG_FLOW
+    //debug_print("[FLOW] Polled.\r\n");
+#endif
     if (sfm3000_poll() == 0) {
         uint32_t curr_time = time_us();
         // TODO divisions by powers of 2 ideally...
@@ -26,13 +39,28 @@ uint8_t poll_volume() {
         volume += volume_inc;
         last_poll_time = curr_time;
         sei();
+#if DEBUG_FLOW
+        if (i == 50) {
+            debug_print("[FLOW] Flow: %li.\r\n", flow);
+            debug_print("[FLOW] Vol: %li.\r\n", volume);
+            i = 0;
+        } else {
+            i++;
+        }
+#endif
         return 0;
     } else {
+#if DEBUG_FLOW
+        //debug_print("[FLOW] Not ready.\r\n");
+#endif
         return 1;
     }
 }
 
 void reset_volume() {
+#if DEBUG_FLOW
+    debug_print("[FLOW] Reset.\r\n");
+#endif
     uint32_t curr_time = time_us();
     cli();
     volume = 0;
