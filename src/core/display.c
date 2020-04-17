0272fc63f81c7662a1b2171618d144a2fdef05c2
anpar <paris.antoine.paris@gmail.com>
2020-04-17 18:07:32 +0200

    Cleaning LCD, part I

--- src/core/display.c
+++ src/core/display.c
@@ -1,13 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include "hal/io.h"
-#include "hal/pins.h"
 #include "hal/lcd.h"
-#include "hal/time.h"
 #include "core/system.h"
 #include "core/display.h"
 #include "core/main_task.h"
@@ -15,247 +14,257 @@
 #include "core/analog_read.h"
 #include "core/alarm.h"
 
-/*
- * TODO:
- * - major clean-up
- * - better buffer management?
- * - more
- */
-
-static void disp_alarm();
-static void disp_muted();
-static void disp_tidal_vol(uint8_t blank);
-static void disp_bpm(uint8_t blank);
-static void disp_extra_param(uint8_t blank);
-static void disp_plateau_p();
-static void disp_peak_p();
-static void disp_state();
-
+#define DEBUG_DISPLAY 0
 #if DEBUG_DISPLAY
 #define DEBUG_PRINT debug_print
 #else
 #define DEBUG_PRINT fake_debug_print
 #endif // DEBUG_DISPLAY
 
-uint32_t muted_switch_time = 0;
-uint8_t muted_msg_on = 0;
+// Time in ticks
+#define MUTE_MSG_PERIOD pdMS_TO_TICKS(2000L)
+#define PARAM_BLINK_PERIOD pdMS_TO_TICKS(250)
 
-uint32_t param_switch_time = 0;
-uint8_t tidal_vol_on = 0;
-uint8_t bpm_on = 0;
-uint8_t extra_param_on = 0;
+static void initDisplay();
+static void displayWelcomeMsg();
+
+// Pressure measurements display
+static void displayPlateauPressure();
+static void displayPeakPressure();
+
+// Information zone display (state, error, mute)
+static void refreshInfoZone();
+static void displayState();
+static void displayErrorCode();
+static void toggleMuteMsg();
+
+// Used for the alternating muted indication
+static TickType_t muteMsgLastVisible = 0;
+static bool muteMsgVisible = false;
+
+// Parameters display
+static void refreshParametersZone();
+static void toggleUnsavedParameters();
+static void displayTidalVolume(bool visible);
+static void displayRespFreq(bool visible);
+static void displayExtraParam(bool visible);
+
+// Used for unconfirmed parameters blinking
+static TickType_t paramLastVisible = 0;
+static bool paramVisible = false;
+
+static void pollAlarmMutedDisplay();
+static void pollUnsavedParamDisplay();
+
+static void processDisplayNotification(uint32_t notification);
 
 void LCDDisplayTask(void *pvParameters)
 {
-    DEBUG_PRINT("[LCD] Starting.\r\n");
+    initDisplay();
+    displayWelcomeMsg();
 
-    // Wait 100ms (otherwhise welcome message not printed)
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));
+    while (true) {
+        pollAlarmMutedDisplay();
+        pollUnsavedParamDisplay();
 
-    lcd_initLCD();
-    lcd_write_string(WELCOME_MSG1, 1, 1, NO_CR_LF);
-    lcd_write_string(WELCOME_MSG2, 2, 1, NO_CR_LF);
+        uint32_t notification = 0;
+        BaseType_t notif_recv = xTaskNotifyWait( 0x0,
+                                                 ALL_NOTIF_BITS,
+                                                 &notification,
+                                                 pdMS_TO_TICKS(100));
 
-    while (1) {
-        uint32_t notification;
-        uint32_t curr_time = time_us();
-
-        // Display "Muted" and the current state/alarm alternatively for 2 seconds if mute_on
-        if (alarmMuted && (globalState == stop || globalState == run)) {
-            if (curr_time - muted_switch_time > 2000000L) {
-                if (!muted_msg_on) {
-                    disp_muted();
-                } else {
-                    if (alarmLevel == noAlarm) {
-                        disp_state();
-                    } else {
-                        disp_alarm();
-                    }
-                }
-
-                muted_msg_on = !muted_msg_on;
-                muted_switch_time = curr_time;
-            }
-        } else {
-            if (muted_msg_on) {
-                muted_msg_on = !muted_msg_on;
-                if (alarmLevel == noAlarm) {
-                    disp_state();
-                } else {
-                    disp_alarm();
-                }
-            }
-       }
-
-        // Make unsaved parameters blink
-        if (unsaved_parameters()) {
-            if (curr_time - param_switch_time > 250000L) {
-                if (saved_tidal_vol != tidal_vol) {
-                    disp_tidal_vol(tidal_vol_on);
-                }
-
-                if (saved_bpm != bpm) {
-                    disp_bpm(bpm_on);
-                }
-
-                if (unsaved_extra_param()) {
-                    disp_extra_param(extra_param_on);
-                }
-
-                param_switch_time = curr_time;
-            }
-        }
-
-        // FIXME: max wait has been changed to implement alternate mute/state/alarm display
-        BaseType_t notif_recv = xTaskNotifyWait(0x0, ALL_NOTIF_BITS, &notification, pdMS_TO_TICKS(100));
-        if (notif_recv == pdTRUE) {
-            if (notification & DISP_NOTIF_PARAM) {
-                DEBUG_PRINT("[LCD] rcvd notif param.\r\n");
-                // FIXME: not clean, for a single param change, we re-write everything
-                disp_tidal_vol(0);
-                disp_bpm(0);
-                disp_extra_param(0);
-
-                if (unsaved_parameters()) {
-                    param_switch_time = time_us();
-                }
-            }
-            if (notification & DISP_NOTIF_PLATEAU_P) {
-                DEBUG_PRINT("[LCD] rcvd notif plateau p.\r\n");
-                disp_plateau_p();
-            }
-            if (notification & DISP_NOTIF_PEAK_P) {
-                DEBUG_PRINT("[LCD] rcvd notif peak p.\r\n");
-                disp_peak_p();
-            }
-            if (notification & DISP_NOTIF_STATE) {
-                DEBUG_PRINT("[LCD] rcvd notif state.\r\n");
-                // Only write the state info if either
-                // - there is no pending alarm (otherwhise alarm info is overwritten)
-                // - globalState is critical_failure FIXME: why?
-                if (alarmLevel == noAlarm || globalState == critical_failure) {
-                    disp_state();
-                }
-            }
-            if (notification & DISP_NOTIF_ALARM) {
-                DEBUG_PRINT("[LCD] rcvd notif ALARM.\r\n");
-                disp_alarm();
-            }
+        if (notif_rcv == pdTRUE) {
+            processDisplayNotification(notification);
         }
 
         lcd_refreshLCD();
     }
 }
 
-static void disp_alarm() {
-    // FIXME: buffer useless here
-    char alarm_buffer[9];
+/*
+ * Initializes the display.
+ */
+static void initDisplay() {
+    DEBUG_PRINT("[LCD] Init.\r\n");
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));
 
-    switch(alarmCause) {
-        case overPressure:
-            sprintf(alarm_buffer, " MXPSR ");
-            break;
-        case noPressure:
-            sprintf(alarm_buffer, " NOPSR ");
-            break;
-        case highPressure:
-            sprintf(alarm_buffer, " HOPSR ");
-            break;
-        case highTemperature:
-            sprintf(alarm_buffer, " MXTPM ");
-            break;
-        case lowPressure:
-            sprintf(alarm_buffer, " LOPSR ");
-            break;
-        case abnVolume:
-            sprintf(alarm_buffer, " TIDALV");
-            break;
-        case abnFreq:
-            sprintf(alarm_buffer, " RESPR ");
-            break;
-        case auxPower:
-            sprintf(alarm_buffer, " AUXPWR");
-            break;
-        case noError:
-            // FIXME: should never happen if above code well design
-            disp_state();
-            break;
-        default:
-            sprintf(alarm_buffer, " ALARM ");
+    lcd_initLCD();
+}
+
+/*
+ * Displays the welcome message.
+ */
+static void displayWelcomeMsg() {
+    lcd_write_string(WELCOME_MSG1, 1, 1, NO_CR_LF);
+    lcd_write_string(WELCOME_MSG2, 2, 1, NO_CR_LF);
+}
+
+/*
+ * If alarm is muted, LCD information zone must alternate between
+ * a muted alarm indication and the current state/error information
+ */
+static void pollAlarmMutedDisplay() {
+    if (alarmMuted) {
+        if (xTaskGetTickCount() - muteMsgLastVisible > MUTE_MSG_PERIOD) {
+            toggleMuteMsg();
+         }
+    } else if (muteMsgVisible) {
+        // Ensures the state/error information is visible when alarm has been unmuted
+        refreshInfoZone();
     }
-
-    lcd_write_string(alarm_buffer, 1, 10, NO_CR_LF);
 }
 
-static void disp_muted() {
-    lcd_write_string(" MUTED ", 1, 10, NO_CR_LF);
-}
-
-// FIXME: better buffer usage
-char tidal_vol_buffer[8];
-static void disp_tidal_vol(uint8_t blank) {
-    if (blank) {
-        sprintf(tidal_vol_buffer, "    ");
-        tidal_vol_on = 0;
+/*
+ * Toggle the appearance of the "MUTED" message in
+ * the information zone of the LCD.
+ */
+static void toggleMuteMsg() {
+    if (muteMsgVisible) {
+        refreshInfoZone();
     } else {
-        sprintf(tidal_vol_buffer, "%2u0 ", tidal_vol);
-        tidal_vol_on = 1;
+        lcd_write_string(" MUTED ", 1, 10, NO_CR_LF);
+        muteMsgLastVisible = xTaskGetTickCount();
     }
 
-    lcd_write_string(tidal_vol_buffer, 2, 1, NO_CR_LF);
+    muteMsgVisible = !muteMsgVisible;
 }
 
-// FIXME: better buffer usage
-char bpm_buffer[8];
-static void disp_bpm(uint8_t blank) {
-    if (blank) {
-        sprintf(bpm_buffer, "   ");
-        bpm_on = 0;
+/*
+ * Displays either the current state or the current
+ * alarm error code, depending on the alarm state.
+ */
+static void refreshInfoZone() {
+    if (alarmLevel == noAlarm) {
+        disp_state();
     } else {
-        sprintf(bpm_buffer, "%2u ", bpm);
-        bpm_on = 1;
+        disp_alarm();
     }
-
-    lcd_write_string(bpm_buffer, 2, 5, NO_CR_LF);
 }
 
-// FIXME: better buffer usage
-char extra_param_buffer[12];
-static void disp_extra_param(uint8_t blank) {
-    if (blank) {
-        sprintf(extra_param_buffer, "         ");
-        extra_param_on = 0;
+/*
+ * Unsaved parameters must blink while unconfirmed
+ */
+static void pollUnsavedParamDisplay() {
+    if (unsaved_parameters()) {
+        if (xTaskGetTickCount() - paramLastVisible > PARAM_BLINK_PERIOD) {
+            toggleUnsavedParameters();
+        }
+    } else if (!paramVisible) {
+        // Ensure parameters are visible when confirmed
+        refreshParametersZone();
+    }
+}
+
+/*
+ * Makes unsaved parameters blink.
+ */
+static void toggleUnsavedParameters() {
+    if (tidal_vol != saved_tidal_vol) {
+        displayTidalVolume(!tidalVolumeVisible);
+    }
+
+    if (bpm != saved_bpm) {
+        displayRespFreq(!respFreqVisible);
+    }
+
+    if (unsaved_extra_param()) {
+        displayExtraParam(!extraParamVisible);
+    }
+
+    paramVisible = !paramVisible;
+}
+
+/*
+ * Displays all parameters and update the paramLastVisible
+ */
+static void refreshParametersZone() {
+    displayTidalVolume(true);
+    displayRespFreq(true);
+    displayExtraParam(true);
+
+    paramVisible = true;
+    paramLastVisible = xTaskGetTickCount();
+}
+
+
+/*
+ * Display tidal volume if visible is true, a blank
+ * space of the corresponding size otherwise.
+ */
+static void displayTidalVolume(bool visible) {
+    char tidalVolumeBuffer[8];
+    if (!visible) {
+        sprintf(tidalVolumeBuffer, "    ");
+    } else {
+        sprintf(tidalVolumeBuffer, "%2u0 ", tidal_vol);
+    }
+
+    lcd_write_string(tidalVolumeBuffer, 2, 1, NO_CR_LF);
+}
+
+/*
+ * Display respiratory frequency if visible is true, a blank
+ * space of the corresponding size otherwise.
+ */
+static void displayRespFreq(bool visible) {
+    char respFreqBuffer[8];
+
+    if (!visible) {
+        sprintf(respFreqBuffer, "   ");
+    } else {
+        sprintf(respFreqBuffer, "%2u ", bpm);
+    }
+
+    lcd_write_string(respFreqBuffer, 2, 5, NO_CR_LF);
+}
+
+/*
+ * Display extra parameter if visible is true, a blank
+ * space of the corresponding size otherwise.
+ */
+static void displayExtraParam(bool visible) {
+    char extraParamBuffer[12];
+
+    if (!visible) {
+        sprintf(extraParamBuffer, "         ");
     } else {
         if (extra_param == 0) {
-            sprintf(extra_param_buffer, "I:E = 1:%1u", ie);
+            sprintf(extraParamBuffer, "I:E = 1:%1u", ie);
         } else if (extra_param == 1) {
-            sprintf(extra_param_buffer, "Pmax = %2u", p_max);
+            sprintf(extraParamBuffer, "Pmax = %2u", p_max);
         } else if (extra_param == 2) {
-            sprintf(extra_param_buffer, "PEEP = %2u", peep);
+            sprintf(extraParamBuffer, "PEEP = %2u", peep);
         }
-
-        extra_param_on = 1;
     }
 
-    lcd_write_string(extra_param_buffer, 2, 8, NO_CR_LF);
+    lcd_write_string(extraParamBuffer, 2, 8, NO_CR_LF);
 }
 
-char plateau_p_buffer[5];
-static void disp_plateau_p() {
-    sprintf(plateau_p_buffer, "PL%2i", p_plateau);
-    lcd_write_string(plateau_p_buffer, 1, 1, NO_CR_LF);
+/*
+ * Display plateau pressure.
+ */
+static void displayPlateauPressure() {
+    char plateauPressureBuffer[5];
+    sprintf(plateauPressureBuffer, "PL%2i", p_plateau);
+    lcd_write_string(plateauPressureBuffer, 1, 1, NO_CR_LF);
 }
 
-char peak_p_buffer[6];
-static void disp_peak_p() {
-    sprintf(peak_p_buffer, "PIC%2i", p_peak);
-    lcd_write_string(peak_p_buffer, 1, 5, NO_CR_LF);
+/*
+ * Display peak pressure.
+ */
+static void displayPeakPressure() {
+    char peakPressureBuffer[6];
+    sprintf(peakPressureBuffer, "PIC%2i", p_peak);
+    lcd_write_string(peakPressureBuffer, 1, 5, NO_CR_LF);
 }
 
-static void disp_state() {
+/*
+ * Display current state in the LCD information zone.
+ */
+static void displayState() {
     if (globalState == critical_failure) {
-        debug_print("[LCD] rcvd crit_fail.\r\n");
+        DEBUG_PRINT("[LCD] rcvd crit_fail.\r\n");
         switch (alarmCause) {
             case doorOpen:
                 lcd_write_string(DOOR_OPEN_MSG, 1, 1, NO_CR_LF);
@@ -302,5 +311,75 @@ static void disp_state() {
         lcd_write_string(" STOP  ",1,10,NO_CR_LF);
     } else if (globalState == run) {
         lcd_write_string(" RUN   ",1,10,NO_CR_LF);
+    }
+}
+
+/*
+ * Displays alarm error code in the LCD information zone.
+ */
+static void displayErrorCode() {
+    // FIXME: buffer useless here
+    char alarm_buffer[9];
+
+    // FIXME: char* [] indexed by alarm Cause containing error codes
+    switch(alarmCause) {
+        case overPressure:
+            sprintf(alarm_buffer, " MXPSR ");
+            break;
+        case noPressure:
+            sprintf(alarm_buffer, " NOPSR ");
+            break;
+        case highPressure:
+            sprintf(alarm_buffer, " HOPSR ");
+            break;
+        case highTemperature:
+            sprintf(alarm_buffer, " MXTPM ");
+            break;
+        case lowPressure:
+            sprintf(alarm_buffer, " LOPSR ");
+            break;
+        case abnVolume:
+            sprintf(alarm_buffer, " TIDALV");
+            break;
+        case abnFreq:
+            sprintf(alarm_buffer, " RESPR ");
+            break;
+        case auxPower:
+            sprintf(alarm_buffer, " AUXPWR");
+            break;
+        case noError:
+            // FIXME: should never happen if above code well design
+            displayState()
+            break;
+        default:
+            sprintf(alarm_buffer, " ALARM ");
+    }
+
+    lcd_write_string(alarm_buffer, 1, 10, NO_CR_LF);
+}
+
+static void processDisplayNotification(uint32_t notification) {
+    // Update parameters
+    if (notification & DISP_NOTIF_PARAM) {
+        DEBUG_PRINT("[LCD] Rcvd NOTIF_PARAM.\r\n");
+        refreshParamZone();
+    }
+
+    // Update plateau pressure
+    if (notification & DISP_NOTIF_PLATEAU_P) {
+        DEBUG_PRINT("[LCD] Rcvd NOTIF_PLATEAU_P.\r\n");
+        displayPlateauPressure();
+    }
+
+    // Update peak pressure
+    if (notification & DISP_NOTIF_PEAK_P) {
+        DEBUG_PRINT("[LCD] Rcvd NOTIF_PEAK_P.\r\n");
+        displayPeakPressure();
+    }
+
+    // Update LCD information zone
+    if (notification & (DISP_NOTIF_STATE | DISP_NOTIF_ALARM)) {
+        DEBUG_PRINT("[LCD] Rcvd NOTIF_STATE|NOTIF_ALARM.\r\n");
+        refreshInfoZone();
     }
 }
